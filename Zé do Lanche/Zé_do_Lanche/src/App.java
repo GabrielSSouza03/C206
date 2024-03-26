3525770714979c0e18b26190a87642736c31e721
Gabriel <127989089+GabrielSSouza03@users.noreply.github.com>
2024-03-26 08:37:07 -0300

    Add files via upload

---
+++ Zé do Lanche/Zé_do_Lanche/src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args)  {
+        
+     int lanche1=10; 
+     int lanche2=4; 
+     int lanche3=2; 
+
+     int soma = lanche1+lanche2+ lanche3 ;
+     float media = soma/3.0f;
+
+
+     System.out.println("total de lanches foi = "+soma);
+     System.out.println("a media de lanches foi " +media);
+    }
+}
